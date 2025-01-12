cd85e729804eb92fff991f0f97412f4dae39afd1
Zakharov Roman <roman.zakharov@ronis.info>
2025-01-12 19:20:45 +0200

    MZ_0004 Add validation to product

--- modules/product/admin.py
+++ modules/product/admin.py
@@ -1,7 +1,13 @@
-from django.contrib import admin
+from typing import Any, Optional
+
+from django.contrib import admin, messages
+from django.db.models import Model
 from django.utils.html import mark_safe, format_html
+from django.http.request import HttpRequest
+from django.utils.safestring import SafeString
 
 from modules.base.forms import get_field_from_info
+from modules.product.choices import ProductStatus
 from modules.product.forms import (
     CategoryChannelFormSet,
     ProductVariantInfoInlineFormSet,
@@ -25,6 +31,7 @@ from modules.product.models import (
     ProductAdditive,
     Product,
     CategoryInfo,
+    Store,
 )
 
 
@@ -50,7 +57,7 @@ class CategoryAdmin(admin.ModelAdmin):
         CategoryInfoInline,
     ]
 
-    def name_from_info(self, obj):
+    def name_from_info(self, obj: Model) -> Optional[str]:
         return get_field_from_info(obj, related_name='info', field_name='name')
 
     name_from_info.short_description = 'Name'
@@ -83,7 +90,7 @@ class ProductVariantAdmin(admin.ModelAdmin):
     list_filter = ('code', 'created_at')
     inlines = [PackageInfoInline]
 
-    def name_from_info(self, obj):
+    def name_from_info(self, obj: Model) -> Optional[str]:
         return get_field_from_info(obj, related_name='info', field_name='name')
 
     name_from_info.short_description = 'Name'
@@ -102,7 +109,7 @@ class AdditiveAdmin(admin.ModelAdmin):
     list_filter = ('created_at', 'updated_at')
     inlines = [AdditiveInfoInline]
 
-    def name_from_info(self, obj):
+    def name_from_info(self, obj: Model) -> Optional[str]:
         return get_field_from_info(obj, related_name='info', field_name='name')
 
     name_from_info.short_description = 'Name'
@@ -120,7 +127,7 @@ class ProductMediaGalleryInline(admin.TabularInline):
     fields = ('image', 'priority', 'image_preview_tag', 'alt_text')
     readonly_fields = ('image_preview_tag',)
 
-    def image_preview_tag(self, obj):
+    def image_preview_tag(self, obj: Model) -> Optional[SafeString]:
         if obj.image and obj.pk:
             return mark_safe(
                 f'<img src="{obj.image.url}" id="preview-{obj.pk}" class="image-preview" style="max-height: 100px; max-width: 100px;" />'
@@ -152,6 +159,16 @@ class ProductVariantInline(admin.TabularInline):
 
 @admin.register(Product)
 class ProductAdmin(admin.ModelAdmin):
+
+    def __init__(self, model, admin_site):
+        super().__init__(model, admin_site)
+        self.has_error = False  # TODO: Temporary flag
+
+    validation_messages = {
+        'create_missing_info': 'Product was created but set to "offline" because ProductInfo is missing for the following stores: {}',
+        'update_missing_info': 'Cannot update to "online": missing ProductInfo for: {}',
+    }
+
     class Media:
         css = {
             'all': ('admin/css/admin_custom.css',)
@@ -174,7 +191,7 @@ class ProductAdmin(admin.ModelAdmin):
         return get_field_from_info(obj, related_name='info', field_name='name')
     name_from_info.short_description = 'Name'
 
-    def preview_media(self, obj):
+    def preview_media(self, obj: Product) -> str:
         url = obj.get_preview_media()
         if url:
             return format_html(
@@ -183,3 +200,49 @@ class ProductAdmin(admin.ModelAdmin):
         return "No Image"
 
     preview_media.short_description = 'Preview'
+
+    def save_related(self, request: HttpRequest, form: Any, formsets: list, change: bool) -> None:
+        super().save_related(request, form, formsets, change)
+
+        product = form.instance
+
+        if product.status == ProductStatus.online:
+            existing_store_ids = set(product.info.values_list('store_id', flat=True))
+
+            formset_store_ids = set()
+            for formset in formsets:
+                if formset.model == ProductInfo:
+                    for inline_form in formset.forms:
+                        if inline_form.cleaned_data and not inline_form.cleaned_data.get('DELETE', False):
+                            formset_store_ids.add(inline_form.cleaned_data['store'].id)
+
+            all_store_ids = existing_store_ids.union(formset_store_ids)
+
+            missing_stores = Store.objects.exclude(id__in=all_store_ids)
+            if missing_stores.exists():
+                missing_store_names = ", ".join(missing_stores.values_list('name', flat=True))
+                if not change:
+                    product.status = ProductStatus.offline
+                    product.save()
+
+                    messages.warning(
+                        request,
+                        self.validation_messages['create_missing_info'].format(missing_store_names)
+                    )
+                else:
+                    # Rollback status to the old value
+                    self.has_error = True
+                    old_status = form.initial.get('status', ProductStatus.offline)
+                    product.status = old_status
+                    product.save()
+
+                    messages.error(
+                        request,
+                        self.validation_messages['update_missing_info'].format(missing_store_names)
+                    )
+
+    def message_user(self, request, message, level=messages.INFO, extra_tags='', fail_silently=False):
+        # TODO: Temporary method
+        if level == messages.SUCCESS and self.has_error:
+            return
+        super().message_user(request, message, level, extra_tags, fail_silently)
